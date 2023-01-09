43833cbea980ff4cb7ee65c9b8f7e1323e875ce2
Leejiwon <wldnjs1277@naver.com>
2023-01-10 02:46:31 +0900

    스토리보드없이 label 만들기

--- FirstIOS/ViewController.swift
+++ FirstIOS/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//선언과 동시에 정의
+    var titlelabel:UILabel = {
+        let label=UILabel()
+        label.text = "hello"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.textColor = .white
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .purple
+        view.addSubview(titlelabel)
+        
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        titlelabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titlelabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
