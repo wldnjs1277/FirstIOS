5deffd4636cd30c3c934314ef07fe6f749f86744
Leejiwon <wldnjs1277@naver.com>
2023-01-30 00:27:20 +0900

    -

--- FirstIOS/ViewControllers/MainViewController.swift
+++ FirstIOS/ViewControllers/MainViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 //선언과 동시에 정의
     var titlelabel:UILabel = {
         let label=UILabel()
-        label.text = "hello"
+        label.text = "메인화면"
         label.textAlignment = .center
         label.font = UIFont.boldSystemFont(ofSize: 50)
         label.textColor = .white
         
         return label
     }()
-    
+
+//뷰가 생성되었을때
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
